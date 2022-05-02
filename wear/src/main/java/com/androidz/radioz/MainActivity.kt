2691726695ab72ed29e2b9387e47ca9a327facb0
Shyam Grover <post2shyam@gmail.com>
2022-05-02 17:50:25 +0530

    Initial commit

---
+++ wear/src/main/java/com/androidz/radioz/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.androidz.radioz
+
+import android.app.Activity
+import android.os.Bundle
+import com.androidz.radioz.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
